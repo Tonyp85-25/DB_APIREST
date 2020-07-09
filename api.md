39bb30a28c785e49cb55865ba311be91166d075b
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2020-07-09 22:08:05 +0200

    api ok

--- api.md
+++ api.md
@@ -4,5 +4,5 @@
 |---|---|---|---|---|
 |Register user|/register|POST|username, password :string| 200 OK|
 |Store sentence|/store|POST|username, password,sentence :string|200 OK, 301 out of tokens,302 Invalid username or password|
-|Retrieve sentence|/get|GET|username, password :string|200 OK, 301 out of tokens,302 Invalid username or password|
+|Retrieve sentence|/get|POST|username, password :string|200 OK, 301 out of tokens,302 Invalid username or password|
 ||||||
--- web/app.py
+++ web/app.py
@@ -26,7 +26,7 @@ class Register(Resource):
 
         username = data["username"]
         password = data["password"]
-        hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt())
+        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
 
         users.insert({
             "Username": username,
@@ -42,6 +42,49 @@ class Register(Resource):
         return jsonify(retJson)
 
 
+def verify_pw(username, password):
+    """
+    checks a user's password
+    :param username: str
+    :param password: str
+    :return: boolean
+    """
+    hashed = users.find({
+        "Username": username,
+    })[0]["Password"]
+
+    if bcrypt.hashpw(password.encode('utf8'), hashed) == hashed:
+        return True
+    else:
+        return False
+
+
+def verify_tokens(username):
+    tokens = users.find({
+        "Username": username
+    })[0]["Tokens"]
+    return tokens
+
+
+def check_user(username, password):
+    # 2. Verify username and password
+    correct_pw = verify_pw(username, password)
+    if not correct_pw:
+        retJson = {
+            "status": 302
+        }
+        return jsonify(retJson)
+
+    # 3. Verify if the user has enough tokens
+    num_tokens = verify_tokens(username)
+    if num_tokens <= 0:
+        retJson = {
+            "status": 301,
+            "msg": "You need to buy some extra tokens"
+        }
+        return jsonify(retJson)
+
+
 class Store(Resource):
     def post(self):
 
@@ -51,22 +94,8 @@ class Store(Resource):
         password = data["password"]
         sentence = data["sentence"]
 
-        # 2. Verify username and password
-        correct_pw = verify_pw(username, password)
-        if not correct_pw:
-            retJson = {
-                "status": 302
-            }
-            return jsonify(retJson)
-
-        # 3. Verify if the user has enough tokens
+        check_user(username, password)
         num_tokens = verify_tokens(username)
-        if num_tokens <= 0:
-            retJson = {
-                "status": 301
-            }
-            return jsonify(retJson)
-
         # 4. store the sentence,take one token and returns ok
         users.update({
             "Username": username}, {
@@ -78,13 +107,43 @@ class Store(Resource):
 
         retJson = {
             "status": 200,
-            "msg": "Sentence saved successfully "
+            "msg": "Sentence saved successfully, you have {0} tokens left".format(num_tokens-1)
         }
         return jsonify(retJson)
-        # hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+class Get(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data["username"]
+        password = data["password"]
+
+        check_user(username, password)
+        num_tokens = verify_tokens(username)
+
+        sentence = users.find({
+            "Username": username
+        })[0]["Sentence"]
+
+        users.update({
+            "Username": username}, {
+            "$set": {
+                "Tokens": num_tokens - 1
+            }
+        })
+
+        retJson = {
+            "status": 200,
+            "sentence": sentence,
+            "msg": "you have {0} tokens left".format(num_tokens-1)
+        }
+        return jsonify(retJson)
 
 
 api.add_resource(Register, '/register')
 api.add_resource(Store, '/store')
+api.add_resource(Get, '/get')
 
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
